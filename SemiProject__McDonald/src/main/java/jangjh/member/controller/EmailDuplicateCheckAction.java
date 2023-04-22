7e91cb0c5e59762f51ef1013ba1d62374a9d3c86
kingking1111d <kingking1111d@gmail.com>
2023-04-22 17:53:12 +0900

    회원가입 생성

---
+++ SemiProject__McDonald/src/main/java/jangjh/member/controller/EmailDuplicateCheckAction.java
@@ -0,0 +1,46 @@
+package jangjh.member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import common.controller.AbstractController;
+import jangjh.member.model.InterMemberDAO;
+import jangjh.member.model.MemberDAO;
+
+public class EmailDuplicateCheckAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String method = request.getMethod(); //"GET" 또는 "POST"
+		
+		if("POST".equalsIgnoreCase(method)) {
+			
+			String email = request.getParameter("email");
+			//System.out.println("~~~ 확인용 email: "+ email);
+			// ~~~ 확인용 email: leess@gmail.com
+
+			InterMemberDAO mdao = new MemberDAO();
+			boolean isExists = mdao.emailDulicateCheck(email);
+			
+			JSONObject jsonObj = new JSONObject();// {}
+			jsonObj.put("isExists", isExists);    // {"isExists": true}
+											      // {"isExists": false}
+			
+			String json = jsonObj.toString(); // 문자열 형태인 "{"isExists": true}" 또는 "{"isExists": false}"으로 만들어준다.
+			
+			//System.err.println(">>> 확인용 json => "+ json);
+			// >>> 확인용 json => {"isExists":true}
+			// >>> 확인용 json => {"isExists":false}
+			
+			request.setAttribute("json", json);
+			
+			//super.setRedirect(false);
+			  super.setViewPage("/WEB-INF/jangjh/jsonview.jsp");
+		}
+		
+	}
+
+}
