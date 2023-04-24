7935adebbfed5777911b43be6b9535487af02932
Jeonyoungmin <jeondudals861@gmail.com>
2023-04-24 18:31:05 +0900

    itemDetailList 수정

--- SemiProject__McDonald/src/main/java/jeonym/item/controller/ItemDetailAction.java
+++ SemiProject__McDonald/src/main/java/jeonym/item/controller/ItemDetailAction.java
@@ -26,60 +26,79 @@ public class ItemDetailAction extends AbstractController {
 		String message = "";
 		String loc = "";
 
-
-		// item_no 가 공백인지 여부
-		if(item_no.trim().isEmpty()) {				
-			
-			message = "item_no는 공백이 될 수 없습니다.";		
-			wrong_data_in_url = true;
-		}
-
-		// int 범위를 넘어서거나 문자등이 들어올 경우
-		try {
 		
-			// item_no 에 음수가 오는지 검사하는 여부
-			if(Integer.parseInt(item_no) < 0) {
-				message = "item_no 는 음수가 될 수 없습니다.";
+		if(item_no != null) {
+			
+			
+			// item_no 가 공백인지 여부
+			if(item_no.trim().isEmpty()) {				
+				message = "item_no는 공백이 될 수 없습니다.";		
+				wrong_data_in_url = true;
+			}
+
+			// int 범위를 넘어서거나 문자등이 들어올 경우
+			try {
+			
+				// item_no 에 음수가 오는지 검사하는 여부
+				if(Integer.parseInt(item_no) < 0) {
+					message = "item_no 는 음수가 될 수 없습니다.";
+					wrong_data_in_url = true;
+				}
+				
+			}
+			catch(NumberFormatException e) { // 유효하지 않은 문자를 item_no 로 입력하는 경우	
+				message = "item_no 가 유효한 값이 아닙니다.";
 				wrong_data_in_url = true;
 			}
 			
-		}
-		catch(NumberFormatException e) { // 유효하지 않은 문자를 item_no 로 입력하는 경우	
-			message = "item_no 가 유효한 값이 아닙니다.";
-			wrong_data_in_url = true;
-		}
-		
-		
-		// item_no 가 존재하는지 여부
-		Map<String, String> paraMap = new HashMap<>();
-		paraMap.put("item_no",item_no);
-		
-		boolean is_Exist_item_no = idao.is_Exist_item_no(paraMap); // item_no 가 DB에 존재하는지 여부 파악
-		
-		if(!is_Exist_item_no) {
-			message = "item_no 가 DB에 존재하지 않습니다.";
-			wrong_data_in_url = true;
-		}
+			
+			// item_no 가 존재하는지 여부
+			Map<String, String> paraMap = new HashMap<>();
+			paraMap.put("item_no",item_no);
+			
+			boolean is_Exist_item_no = idao.is_Exist_item_no(paraMap); // item_no 가 DB에 존재하는지 여부 파악
+			
+			if(!is_Exist_item_no) {
+				message = "item_no 가 DB에 존재하지 않습니다.";
+				wrong_data_in_url = true;
+			}
 
-		if(!wrong_data_in_url) {
+			if(!wrong_data_in_url) {
+				
+				ItemDetailVO idvo = idao.selectOneItem(paraMap);
+				
+				request.setAttribute("idvo", idvo);
+				
+				setRedirect(false);
+				setViewPage("/WEB-INF/jeonym/item/itemDetailList.jsp");
+				
+			}
+			else {
+
+				loc = "javascript:history.back()";
+				request.setAttribute("message", message);
+				request.setAttribute("loc", loc);
+				
+				setRedirect(false);
+				setViewPage("/WEB-INF/jeonym/msg.jsp"); // error 메시지
+			}
 			
-			ItemDetailVO idvo = idao.selectOneItem(paraMap);
 			
-			request.setAttribute("idvo", idvo);
-			
-			setRedirect(false);
-			setViewPage("/WEB-INF/jeonym/item/itemDetailList.jsp");
 			
 		}
-		else {
-
+		else { // item_no 가 null 이라면
+			
+			message = "item_no 는 null 이 될 수 없습니다.";
 			loc = "javascript:history.back()";
 			request.setAttribute("message", message);
 			request.setAttribute("loc", loc);
 			
 			setRedirect(false);
 			setViewPage("/WEB-INF/jeonym/msg.jsp"); // error 메시지
+
+			
 		}
+
 		
 	}
 }
