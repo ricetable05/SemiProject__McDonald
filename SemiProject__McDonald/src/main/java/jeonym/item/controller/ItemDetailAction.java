667cd874b4ff526eafd4eb93fbc497c63aaa58bc
Jeonyoungmin <jeondudals861@gmail.com>
2023-05-03 15:51:23 +0900

    itemDetail 수정

--- SemiProject__McDonald/src/main/java/jeonym/item/controller/ItemDetailAction.java
+++ SemiProject__McDonald/src/main/java/jeonym/item/controller/ItemDetailAction.java
@@ -27,8 +27,10 @@ public class ItemDetailAction extends AbstractController {
 		
 			InterItemDAO idao = new ItemDAO();
 			
+			
+			Map<String, String> paraMap = new HashMap<>();
 			boolean wrong_data_in_url = false; // url 에 잘못된 값을 입력했는지 여부 알아오기
-			String item_no = request.getParameter("item_no");
+			String item_no = request.getParameter("item_no").trim();
 			
 			String message = "";
 			String loc = "";
@@ -36,40 +38,65 @@ public class ItemDetailAction extends AbstractController {
 			
 			if(item_no != null) {
 				
-				
-				// item_no 가 공백인지 여부
-				if(item_no.trim().isEmpty()) {				
-					message = "item_no는 공백이 될 수 없습니다.";		
-					wrong_data_in_url = true;
-				}
+			try {
 	
-				// int 범위를 넘어서거나 문자등이 들어올 경우
-				try {
-				
-					// item_no 에 음수가 오는지 검사하는 여부
-					if(Integer.parseInt(item_no) < 0) {
-						message = "item_no 는 음수가 될 수 없습니다.";
+					// item_no 가 공백인지 여부
+					if(item_no.trim().isEmpty()) {				
+						message = "item_no는 공백이 될 수 없습니다.";		
 						wrong_data_in_url = true;
 					}
-					
+					// item_no 에 음수가 오는지 검사하는 여부										
+					else if(Integer.parseInt(item_no) < 0) {
+						
+						message = "item_no 는 음수가 될 수 없습니다.";
+						loc = "javascript:history.back()";
+						request.setAttribute("message", message);
+						request.setAttribute("loc", loc);
+						
+						setRedirect(false);
+						setViewPage("/WEB-INF/jeonym/msg.jsp"); // error 메시지
+						
+						return;		
+					}
+
+					paraMap.put("item_no",item_no);
+					boolean is_Exist_item_no = idao.is_Exist_item_no(paraMap); // item_no 가 DB에 존재하는지 여부 파악
+
+					if(!is_Exist_item_no) {
+						message = "item_no 가 DB에 존재하지 않습니다.";
+						wrong_data_in_url = true;
+					}
+
 				}
 				catch(NumberFormatException e) { // 유효하지 않은 문자를 item_no 로 입력하는 경우	
-					message = "item_no 가 유효한 값이 아닙니다.";
-					wrong_data_in_url = true;
+				
+					boolean isDigit = true;
+					
+					char charArr[] = item_no.trim().toCharArray();
+					for(int i=0;i<charArr.length; i++) {
+						
+						if(!Character.isDigit(charArr[i])) {
+							isDigit = false;
+							break;
+						}
+						
+					}
+					
+					if(isDigit) {
+						
+						message = "item_no 의 범위가 int 를 벗어났습니다.";
+						wrong_data_in_url = true;
+						
+					}
+					else {
+						
+						message = "item_no는 문자형태가 올 수 없습니다.";
+						wrong_data_in_url = true;
+						
+					}
+
 				}
-				
-				
-				// item_no 가 존재하는지 여부
-				Map<String, String> paraMap = new HashMap<>();
-				paraMap.put("item_no",item_no);
-				
-				boolean is_Exist_item_no = idao.is_Exist_item_no(paraMap); // item_no 가 DB에 존재하는지 여부 파악
-				
-				if(!is_Exist_item_no) {
-					message = "item_no 가 DB에 존재하지 않습니다.";
-					wrong_data_in_url = true;
-				}
-	
+					
 				if(!wrong_data_in_url) {
 					
 					ItemDetailVO idvo = idao.selectOneItem(paraMap);
